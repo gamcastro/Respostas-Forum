ddae476532855eef9a0a998b4288565e27e7f020
George Castro <gamcastro@bol.com.br>
2023-04-04 04:39:49 -0300

    first commit

---
+++ Powershell-Sort-By-Uniqueness-of-the-Hostname.ps1
@@ -0,0 +1,8 @@
+﻿[CmdletBinding()]
+param()
+
+$hostname = @('DSERVER01', 'DSERVER02', 'PSERVER03', 'PSERVER04', 'DSERVER05', 'PSERVER01')
+foreach ($ho in $hostname) {
+    Write-Host "É o nome $ho"
+}
+
