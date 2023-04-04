252642eb45a6d4252a4879d11e6487291e0b125c
George Castro <gamcastro@bol.com.br>
2023-04-04 04:52:21 -0300

    Colocado comentários no código.

--- Powershell-Sort-By-Uniqueness-of-the-Hostname.ps1
+++ Powershell-Sort-By-Uniqueness-of-the-Hostname.ps1
@@ -1,4 +1,6 @@
-﻿[CmdletBinding()]
+﻿# Pergunta no forum stackoverflow
+# https://stackoverflow.com/questions/75925763/powershell-sort-by-uniqueness-of-the-hostname
+[CmdletBinding()]
 param()
 
 $hostname = @('DSERVER01', 'DSERVER02', 'PSERVER03', 'PSERVER04', 'DSERVER05', 'PSERVER01')
@@ -6,3 +8,4 @@ foreach ($ho in $hostname) {
     Write-Host "É o nome $ho"
 }
 
+# Criar uma ferramenta para a resposta
