b12e7b97f7b313475d2a9865c1c90e0757dffc3a
George Castro <gamcastro@bol.com.br>
2023-04-12 00:48:06 -0300

    Respondido.

--- Powershell-Sort-By-Uniqueness-of-the-Hostname.ps1
+++ Powershell-Sort-By-Uniqueness-of-the-Hostname.ps1
@@ -3,9 +3,34 @@
 [CmdletBinding()]
 param()
 
-$hostname = @('DSERVER01', 'DSERVER02', 'PSERVER03', 'PSERVER04', 'DSERVER05', 'PSERVER01')
-foreach ($ho in $hostname) {
-    Write-Host "É o nome $ho"
-}
+$ht = @{}
 
-# Criar uma ferramenta para a resposta
+$o = new-object psobject -property @{
+    server    = 'DSERVER01'
+    bootgroup = 'A'
+}
+$ht.Add($o.server, $o)
+
+$o = new-object psobject -property @{
+    server    = 'DSERVER02'
+    bootgroup = 'B'
+}
+$ht.Add($o.server, $o)
+
+$o = new-object psobject -property @{
+    server    = 'PSERVER03'
+    bootgroup = 'C'
+}
+$ht.Add($o.server, $o)
+
+$o = new-object psobject -property @{
+    server    = 'PSERVER04'
+    bootgroup = 'A'
+}
+$ht.Add($o.server, $o)
+
+# Now when you want to get a list of servers in boot group A, filter the collection like so,
+$ht.GetEnumerator() | Where-Object {$_.Value.bootgroup -eq 'A'}
+
+# And if all you need is the server name, pipe it to select-object.
+$ht.GetEnumerator() | Where-Object {$_.Value.bootgroup -eq 'A'} | Select-Object -Property name
